f0e554624bb2ec59ca4608d8b326ade205b5670c
Emilio Depena <emiliosdep@gmail.com>
2019-10-24 18:47:14 -0400

    Convert Celcius to Fahrenheit

---
+++ celciustofahrenheit.js
@@ -0,0 +1,9 @@
+// Convert Celcius to Fahrenheit
+
+function convertToFahrenheit(celcius) {
+    var fahrenheit = (celcius * (9/5)) + 32;
+    return fahrenheit;
+    console.log(fahrenheit)
+}
+
+convertToFahrenheit(30);
