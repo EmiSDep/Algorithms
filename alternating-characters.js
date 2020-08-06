09ed6c5e705fef75b42466ec87a6a623a59d841b
Emilio Depena <emiliosdep@gmail.com>
2020-08-06 18:55:38 -0400

    alternating characters

---
+++ alternating-characters.js
@@ -0,0 +1,16 @@
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+    
+    const stringArr = s.split("");
+    let deletions = 0;
+    
+    // console.log(3, stringArr);
+    stringArr.forEach((letter, index) => {
+        // console.log(`Current Letter: ${letter} Next Letter: ${stringArr[index + 1]}`)
+        if (letter === stringArr[index + 1]) {
+            ++deletions;
+        }
+    });
+​
+    // console.log(`Deletions: ${deletions}`);
+    return deletions;
