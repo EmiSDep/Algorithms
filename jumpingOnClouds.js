e243beab1711b4d9f7e538f82f131c1adfcc18fb
Emilio Depena <emiliosdep@gmail.com>
2020-08-13 18:55:47 -0400

    jumping on clouds

---
+++ jumpingOnClouds.js
@@ -0,0 +1,10 @@
+function jumpingOnClouds(c) {
+    let jump = 0;
+    for ( let i = 0; i < c.length -1; i +=2) {
+        jump++;
+        if(c[i] == 1) {
+            i -= 1;
+        } 
+    }
+    return jump;
+}
